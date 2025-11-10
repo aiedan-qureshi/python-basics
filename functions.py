9779ca1b98469134d00e5e7879f4f8f6fac0ed58
Aiedan Qureshi <qureshiaiedan.professional@gmail.com>
2025-11-10 11:23:28 +0500

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,11 @@
+# functions.py
+# Demonstrates functions and return values
+
+def greet(name):
+    return f"Hello, {name}!"
+
+def add(a, b):
+    return a + b
+
+print(greet("Aiedan"))
+print("Sum of 5 + 3 =", add(5, 3))
